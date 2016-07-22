ca0c752b6fdd42b8f8654b8bc2cfed044f9fcdab
moizest89 <moises.gilberto@gmail.com>
2016-07-21 21:41:43 -0600

    devise, basic implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+  resources :roles
+
+
+  devise_for :users
+  scope "/admin" do
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+r1 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
+r2 = Role.create({name: "Technical", description: "Can read ticket and upload PDF with the results."})
+r3 = Role.create({name: "Tenant", description: "Can create ticket for technical support"})
+
+User.create({name: "Moises Portillo", email: "admin@example.com", password: "1234567890", password_confirmation: "1234567890", role_id: r1.id})
+User.create({name: "Jorge Escalante", email: "jorge@example.com", password: "1234567890", password_confirmation: "1234567890", role_id: r2.id})
+User.create({name: "Kevin Argumedo", email: "kevin@example.com", password: "1234567890", password_confirmation: "1234567890", role_id: r3.id})
+
